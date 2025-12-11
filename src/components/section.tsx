787d7a31e1fc8b0734f8489cfc7733832dace07f
Jasson-01 <jason.davila001@gmail.com>
2025-12-10 23:33:56 -0300

    Taller2-Clase-Fecha
    Terminado

---
+++ src/components/section.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+const Section = () => {
+    return <div className="grid place-items-center h-screen content-center">
+        <Button>Button</Button>
+    </div>;
+};
+
+export default Section;
